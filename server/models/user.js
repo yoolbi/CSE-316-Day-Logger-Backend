3d53f89568403fbe6e264e7db654a3dcfd0abdd2
Yool Bi Lee <yoolbi.lee@stonybrook.edu>
2021-12-03 23:56:52 +0900

    Revised user schema

--- server/models/user.js
+++ server/models/user.js
@@ -3,11 +3,6 @@ const bcrypt = require('bcrypt')
 
 var Schema = mongoose.Schema;
 
-var validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
-
 var UserSchema = new Schema(
     {
         name: {type: String, required: true},
@@ -16,13 +11,12 @@ var UserSchema = new Schema(
             required: true,
             trim: true,
             unique: true,
-            validate: [validateEmail, 'Please fill a valid email address'],
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
         password: {
             type: String,
             required: true,
-            minlength: 6
+            match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, 'Please fill a valid password']
         },
         profile_url: {type: String},
         address: {
