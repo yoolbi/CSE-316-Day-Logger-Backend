e3c4e9962fa54e2ad7aa074cf05429a35905f6ea
Yool Bi Lee <yoolbi.lee@stonybrook.edu>
2021-12-08 00:39:33 +0900

    deploy

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
     {
-        name: {type: String, required: true},
+        name: { type: String, required: true },
         email: {
             type: String,
             required: true,
